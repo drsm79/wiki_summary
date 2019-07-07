f10dbdb50e3d8fce48a4f708852c1bda7c00ef32
Simon Metson <simonmetson+github@googlemail.com>
2019-07-07 14:21:40 +0100

    Create wiki_summary.py

---
+++ wiki_summary.py
@@ -0,0 +1,30 @@
+import os
+import argparse
+
+def list_dir(the_dir):
+    entries = os.listdir(the_dir)
+    return filter(lambda x: x[0] not in ('.', '_'), entries)
+    # [x for x in entries if not x.startswith('.') or not x.startswith('_')]
+
+def read_files(the_dir):
+    contents = {}
+    for a_file in list_dir(the_dir):
+        path = os.path.join(the_dir, a_file)
+        with open(path) as f:
+            lines = list(filter(lambda x: x not in ('\n'), f.readlines()))
+            for skip in ['_**Note:', 'https://yout', '**_Note:']:
+                if lines[0].startswith(skip):
+                    lines.pop(0)
+            contents[a_file.replace('.md', '')] = lines[0]
+    return contents
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('dir', metavar='DIR', help='The directory to scan.')
+    args = parser.parse_args()
+    file_contents = read_files(args.dir)
+    for k in sorted(file_contents.keys()):
+        print(k, ':', file_contents[k])
+
+if __name__ == '__main__':
+    main()
